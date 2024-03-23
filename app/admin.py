d952b2d32a10de2f0eb403e68d30762eaaee0b26
Mariane Freitas <mariane11.freitas@gmail.com>
2024-03-23 13:14:08 -0300

    add models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Aluno)
+admin.site.register(Professor)
+admin.site.register(Categoria)
+admin.site.register(Turma)
+admin.site.register(HistoricoMedicoes)
+admin.site.register(ProfessorTurma)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django import utils
+import datetime
 
-# Create your models here.
+class Turma(models.Model):
+    codigoTurma = models.CharField('Codigo Turma', max_length=8)
+    dataInicio = models.DateField('Data Inicio')
+    descricao = models.CharField('Descricao', max_length=8)
+
+    # class Meta:
+    #     verbose_name = 'Turma'
+
+class Aluno(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    sexo = models.CharField('Sexo', max_length=1)
+    dataNascimento = models.DateField('Data Nascimento')
+    ra = models.CharField('RA', max_length=8)
+    turmaAtual = models.ForeignKey(Turma, on_delete=models.CASCADE)
+
+
+class Professor(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    numeroRegistro = models.CharField('Numero registro', max_length= 8)
+    email = models.CharField('E-mail', max_length=100)
+    senha = models.CharField('Senha', max_length=8) 
+    
+    # class Meta:
+    #     verbose_name = 'Professor'
+
+class ProfessorTurma(models.Model):
+    id_professor = models.ForeignKey(Professor, on_delete=models.CASCADE)   
+    id_aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+
+class Categoria(models.Model):
+    categoria_nome = models.CharField('Categoria', max_length=20) 
+    
+class HistoricoMedicoes(models.Model):
+    id_aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    altura = models.FloatField('Altura', max_length=100)
+    peso = models.FloatField('Peso')
+    imc = models.FloatField('IMC')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    timeStamp = models.DateField('TS', default= utils.timezone.now)   
+
