961b8be8a036359b88691db3b92ff04f848b4f84
Mariane Freitas <mariane11.freitas@gmail.com>
2024-04-10 00:24:36 -0300

    Add login e logout

--- IMC/settings.py
+++ IMC/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
+    'django_bootstrap5'
 ]
 
 MIDDLEWARE = [
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    
+    path('', views.home, name='home'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
