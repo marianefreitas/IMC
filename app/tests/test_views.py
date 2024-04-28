d88e27430b333af306fade132bbda9a7c2e7b486
Mariane Freitas <mariane11.freitas@gmail.com>
2024-04-27 23:52:30 -0300

    removendo testes da view

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -7,19 +7,19 @@
 #     def setUp(self):
 #         self.client = Client()
 
-    # def test_home(self):
-    #     response = self.client.get(reverse('home'))
+#     def test_home(self):
+#         response = self.client.get(reverse('home'))
 
-    #     self.assertAlmostEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'index.html')
+#         self.assertAlmostEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'index.html')
 
-    # def test_login(self):
-    #     response = self.client.get(reverse('login'))
+#     def test_login(self):
+#         response = self.client.get(reverse('login'))
 
-    #     self.assertAlmostEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'login.html')
+#         self.assertAlmostEqual(response.status_code, 200)
+#         self.assertTemplateUsed(response, 'login.html')
 
-    # def test_logout(self):
-    #     response = self.client.get(reverse('logout'))
+#     def test_logout(self):
+#         response = self.client.get(reverse('logout'))
 
-    #     self.assertAlmostEqual(response.status_code, 302)
+#         self.assertAlmostEqual(response.status_code, 302)
