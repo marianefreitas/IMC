90b4130d8f8a0035ca83806cb5ba302adabaf173
Mariane Freitas <mariane11.freitas@gmail.com>
2024-04-27 23:49:26 -0300

    removendo teste views

--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -19,7 +19,7 @@ class TestViews(TestCase):
     #     self.assertAlmostEqual(response.status_code, 200)
     #     self.assertTemplateUsed(response, 'login.html')
 
-    def test_logout(self):
-        response = self.client.get(reverse('logout'))
+    # def test_logout(self):
+    #     response = self.client.get(reverse('logout'))
 
-        self.assertAlmostEqual(response.status_code, 302)
+    #     self.assertAlmostEqual(response.status_code, 302)
