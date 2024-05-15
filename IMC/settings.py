88a9f991847336da597df72d0b4a236e0f276e6c
Patrick Almeida <patricklucas2012@gmail.com>
2024-05-15 18:47:06 -0300

    update deploy

--- IMC/settings.py
+++ IMC/settings.py
@@ -79,13 +79,6 @@ WSGI_APPLICATION = 'IMC.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-} """
 
 if 'test' in sys.argv:
     DATABASES = {
@@ -96,14 +89,6 @@ if 'test' in sys.argv:
     }
 else:
     DATABASES = {
-        # 'default': {
-        #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #     'NAME': 'ddukr6dggksq0c',
-        #     'USER': 'luvmxrnjejbvfy',
-        #     'PASSWORD': 'ef64aaf70427e7b8c5066c0d4ad94ef23f46d94cb69ef73b08bb0e8c386ffade',
-        #     'HOST': 'ec2-52-54-200-216.compute-1.amazonaws.com',
-        #     'PORT': '5432',
-        # }
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'dficcrtdd7iie1',
@@ -112,10 +97,6 @@ else:
             'HOST': 'cb5ajfjosdpmil.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
             'PORT': '5432',
         }
-        # 'default': {
-        #     'ENGINE': 'django.db.backends.sqlite3',
-        #     'NAME': BASE_DIR / 'db.sqlite3',
-        # }
     }
 
 
